193138a6af6b0838d1eb2e1873e2ec82370ff744
Jakub Sokołowski <panswiata@gmail.com>
2017-12-15 18:04:11 +0000

    add moar data to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rbackup",
-    version="0.1",
+    name='rbackup',
+    version='0.1',
     packages=find_packages(),
+
+    author='Some Dude',
+    author_email='i@always.fail',
+    description='Utility for automating backups with tar & rsync.',
+    license='PSF',
+    keywords='backup rsync tar tarball automating',
+
+    scripts=['rbackup'],
+    entry_points={
+        'console_scripts': [
+            'rbackup = rbackup.script:main'
+        ],
+    },
+
+    # dependencies
+    install_requires=[
+        'sh>=1.12',
+    ],
+    setup_requires=[
+        'pytest>=2'
+    ]
 )
