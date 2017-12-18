b3447e789bed2caa68234f4ad2b3c41a71b3a662
Jakub Sokołowski <panswiata@gmail.com>
2017-12-18 22:33:36 +0100

    move fixtures to conftest

--- test/test_sync.py
+++ test/test_sync.py
@@ -1,44 +1,9 @@
 import logging
-from pytest import mark, fixture, raises
+from pytest import mark, raises
 from mock import patch, Mock
 
-from rbackup.asset import Asset
-from rbackup.target import Target
 from rbackup import sync
 
-@fixture
-def rsync_output():
-    return [
-        'Number of files: 53,051 (reg: 45,676, dir: 7,246, link: 129)',
-        'Number of created files: 0',
-        'Number of deleted files: 0',
-        'Number of regular files transferred: 13',
-        'Total file size: 360.77M bytes',
-        'Total transferred file size: 421.69K bytes',
-        'Literal data: 421.69K bytes',
-        'Matched data: 0 bytes',
-        'File list size: 1.18M',
-        'File list generation time: 1.024 seconds',
-        'File list transfer time: 0.000 seconds',
-        'Total bytes sent: 1.67M',
-        'Total bytes received: 283',
-        'sent 1.67M bytes  received 283 bytes  1.11M bytes/sec',
-        'total size is 360.77M  speedup is 216.61',
-    ]
-
-@fixture
-def asset():
-    return Asset(
-        'asset_id',
-        '/asset/path',
-        target='target_id',
-        timeout=666,
-        dest='bkp'
-    )
-
-@fixture
-def target():
-    return Target('target_id', '/target/path', host='bkp.example.net', user='testuser')
 
 @patch('rbackup.sync.LOG')
 def test__log_rsync_stats(m_log, rsync_output):
